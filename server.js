c2fe7edebc0cc597d6688636829a05f46bcc39e0
Paul Chin Jr <pchinjr@gmail.com>
2018-01-11 13:23:55 -0500

    front end stuff

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
-var express = require('express');
-var app = express();
-var server = require("http").createServer(app);
-var io = require('socket.io')(server);
-var port = 3000;
+const express = require('express');
+const app = express();
+const server = require("http").createServer(app);
+const io = require('socket.io')(server);
+const port = 3000;
+const temporal = 
 
-app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res) {
-        res.sendFile(__dirname + '/public/index.html');
-});
 server.listen(port);
 console.log('Server listening at http://localhost:' + port);
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+
+const RollingSpider = require('rolling-spider');
+const rollingSpider = new RollingSpider();
+
+rollingSpider.connect( function() {
+    rollingSpider.setup( function () {
+        rollingSpider.flatTrim();
+        rollingSpider.startPing();
+        rollingSpider.flatTrim();
+        console.log('Connected to drone ', rollingSpider.name);
+    })
+})
+
+
+//SocketIO connection handler and listening
+io.on('connection', function (socket) {
+  console.log("your socket id is ", socket.id);
+
+  socket.on('take-off', function(data) {
+    console.log('takeoff clicked');
+    rollingSpider.takeOff();
+  });
+
+  socket.on('land', function(data) {
+    console.log('land clicked');
+    rollingSpider.land();
+  });
+
+  socket.on('disconnect', function() {
+    console.log('browser closed');
+  })
+});
